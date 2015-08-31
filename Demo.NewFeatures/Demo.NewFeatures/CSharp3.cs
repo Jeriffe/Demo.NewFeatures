3f3c606d9c0f12c47f1e487c54e19837ebab2de5
蒋飞 <jiang-fei12356@126.com>
2015-08-31 10:02:01 +0800

    Initial project

---
+++ Demo.NewFeatures/Demo.NewFeatures/CSharp3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.NewFeatures
+{
+   public class CSharp3
+    {
+        //Auto properties
+        public int Age { get; set; }
+
+    }
+}
