a3b6bdb4ee327d2c9c5e82c006d5dec1271a2997
Jeriffe Jiang <jjiang@touchpointmed.com>
2023-11-16 13:55:35 +0800

    Updated C#11 features

--- Demo.NewFeatures/Demo.Feature_CSharp11/CSharp11.cs
+++ Demo.NewFeatures/Demo.Feature_CSharp11/CSharp11.cs
@@ -1,136 +1,264 @@
 ﻿global using Demo.Feature_CSharp.Infrastructure;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
-//File Namespaces
-//With this feature you can define the namespace on the file level like this:
 namespace Demo.Feature_CSharp;
-
 
 public class CSharp11 : ICSharp
 {
     public void ShowNewFeatures()
     {
         Console.WriteLine($"**************begin {this.GetType().Name} new features********************");
-        
-        /*  Raw string literals
-            Generic math support
-            Generic attributes
-            UTF-8 string literals
-            Newlines in string interpolation expressions
-            List patterns
-            File-local types
-            Required members
-            Auto-default structs
-            Pattern match Span<char> on a constant string
-            Extended nameof scope
-            Numeric IntPtr
-            ref fields and scoped ref
-            Improved method group conversion to delegate
-            Warning wave 7
+
+        //Static abstract members in interfaces
+        StaticAbstractMembersInInterfaces();
+
+        //Raw string literals
+        RawStringLiterals();
+
+        // Generic math support
+
+        // Generic attributes
+        GenericAttributes();
+        //UTF - 8 string literals
+        UTF_8StringLiterals();
+
+        // Newlines in string interpolation expressions
+        NewlinesInStringInterpolationExpressions();
+
+        // List patterns
+        ListPatterns();
+
+        /* File-local types
+         * 
+         * you can use the file access modifier to create a type whose visibility is scoped to the source file 
+         * in which it is declared.This feature helps source generator authors avoid naming collisions.
         */
+
+        // Required members
+        RequiredMembers();
+        //  Auto-default structs
+        AutoDefaultStructs();
+        //  Pattern match Span<char> on a constant string
+        
+        //  Extended nameof scope
+        ExtendedNameofScope();
+
+        //  Numeric IntPtr
+
+        /*  ref fields and scoped ref
+         *  
+         *You can declare ref fields inside a ref struct. 
+         *This supports types such as System.Span<T> without special attributes or hidden internal types.
+         */
+
 
         Console.WriteLine($"********************end {this.GetType().Name} new features********************");
     }
-    private void RecordStruct()
-    {
-        /*结构类型的改进
-            可以在结构类型中声明实例无参数构造函数，并在声明实例字段或属性时对它们进行初始化。 
-            with 表达式的左侧操作数可以是任何结构类型。
-        */
 
-        /*
-         *  older        -->  public record RPerson(string FirstName, string LastName);
-         *  new in c# 10 -->  public readonly record struct RPoint(double X, double Y, double Z);
+    // Interface specifies static properties and operators
+    public interface IAddable<T> where T : IAddable<T>
+    {
+        static abstract T Zero { get; }
+        static abstract T operator +(T t1, T t2);
+    }
+
+    // Generic algorithms can use static members on T
+    public static T AddAll<T>(T[] ts) where T : IAddable<T>
+    {
+        T result = T.Zero;                   // Call static operator
+        foreach (T t in ts) { result += t; } // Use `+`
+        return result;
+    }
+
+    private void StaticAbstractMembersInInterfaces()
+    {
+        /*There is currently no way to abstract over static members 
+         * and write generalized code that applies across types that define those static members. 
+         * This is particularly problematic for member kinds that only exist in a static form, notably operators.
          */
     }
 
-    private void GlobalUsingDirective()
+    public class MyAttr : Attribute
     {
-        /*C# 10 introduces is global. Using global, you will be able to define global usings for the whole project. 
-         * In general, it is recommended to create a separate file which will contain these imports, something like usings.cs.
-         * 
-         * global using Demo.Feature_CSharp.Infrastructure;
-         * global using System.Linq;
-         */
-    }
+        private readonly string _paramName;
 
-    private void FileNamespaces()
-    {
-        /*
-              namespace Demo.Feature_CSharp10
-              {
-                public class CSharp10
-                {
-                  ...
-                }
-              }
-
-             //new in c# 10
-              namespace Demo.Feature_CSharp10;
-
-              public class CSharp10
-              {
-                ...
-              }
-       */
-    }
-
-    private void ExtendedPropertyPatterns()
-    {
-        /*从 C# 10 开始，可引用属性模式中嵌套的属性或字段。 例如，窗体的模式
-         * { Prop1: { Prop2: pattern } }
-         */
-
-        /*对 Lambda 表达式的改进
-         * Lambda 表达式可以具有自然类型，这使编译器可从 Lambda 表达式或方法组推断委托类型。
-         * 如果编译器无法推断返回类型，Lambda 表达式可以声明该类型。
-         * 属性可应用于 Lambda 表达式。
-        */
-
-        var p = new Person { Address = new Address { Street = "A street" } };
-
-        //older
-        if (p is Person { Address: { Street: "A street" } }) { }
-
-        //new in C# 10
-        if (p is Person { Address.Street: "A street" }) { }
-
-    }
-
-    private void ConstantInterpolatedStrings()
-    {
-        //older
-        string ThankYouMessage1()
+        public MyAttr(string paramName)
         {
-            var message = "Thank you for buying the book!";
-            var thankYouMessage = $"{message} Enjoy";
-
-            return thankYouMessage;
-        }
-
-        //new in c# 10
-        string ThankYouMessage2()
-        {
-            const string message = "Thank you for buying the book!";
-            const string thankYouMessage = $"{message} Enjoy";
-
-            return thankYouMessage;
+            _paramName = paramName;
         }
     }
-}
+    public class MyClass_ExtendedNameofScope
+    {
+        [MyAttr(nameof(param))]
+        public void Method_ExtendedNameofScope(int param, [MyAttr(nameof(param))] int anotherParam)
+        { }
+    }
+    private void ExtendedNameofScope()
+    {
+        new MyClass_ExtendedNameofScope().Method_ExtendedNameofScope(0, 1);
+    }
 
-public record RPerson(string FirstName, string LastName);
-public readonly record struct RPoint(double X, double Y, double Z);
+    //GenericAttributes
+    class MyType { }
+    class GenericAttribute<T> : Attribute where T : MyType { private T _type; }
+    [GenericAttribute<MyType>]
+    class MyClass { }
 
-class Person
-{
-    public string FirstName { get; set; }
+    private void GenericAttributes()
+    {
+        //C#10
+        /*
+         class MyType { }
 
-    public Address Address { get; set; }
-}
+         class MyAttribute : Attribute
+         {
+             private Type type;
+             public MyAttribute(Type type) { _type = type; }
+         }
 
-public class Address
-{
-    public string Street { get; set; }
+         [MyAttribute(typeof(MyType))]
+         class Myclass { }
+        */
+    }
 
-    public string ZipCode { get; set; }
+    // before C#11 doesn't complied -- Auto-implemented property 'Person.Age  must be fully assigned before control is returned to the caller. 
+    struct Person_AutoDefaultStructs
+    {
+        public Person_AutoDefaultStructs(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    private void AutoDefaultStructs()
+    {
+        var p = new Person_AutoDefaultStructs("Gray");
+        p.Age = 28;
+    }
+
+    private void NewlinesInStringInterpolationExpressions()
+    {
+        //Allows any valid C# code between { }, including newlines, to improve code readability
+        // switch expression in string interpolation 
+        int month = 5;
+        string season = $"The season is {month switch
+        {
+            1 or 2 or 12 => "winter",
+            > 2 and < 6 => "spring",
+            > 5 and < 9 => "summer",
+            > 8 and < 12 => "autumn",
+            _ => "Unknown. Wrong month number",
+        }}.";
+
+        Console.WriteLine(season);
+        // The season is spring.
+
+        // LINQ query in string interpolation 
+        int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+        string message = $"The reversed even values of {nameof(numbers)} are {string.Join(", ", numbers.Where(n => n % 2 == 0)
+              .Reverse())}.";
+
+        // The reversed even values of numbers are 6, 4, 2.
+        Console.WriteLine(message);
+    }
+
+    private void ListPatterns()
+    {
+        /*Expand pattern matching to match sequences of elements in a list or an array.
+         * You can use list patterns with any pattern(including property, type, constant, &relational patterns).
+         */
+        var numbers = new[] { 1, 2, 3, 4 };
+        // List and constant patterns 
+        Console.WriteLine(numbers is [1, 2, 3, 4]); // True 
+        Console.WriteLine(numbers is [1, 2, 4]); // False
+
+        // List and discard patterns 
+        Console.WriteLine(numbers is [_, 2, _, 4]); // True 
+        Console.WriteLine(numbers is [.., 3, _]); // True
+
+        // List and logical patterns 
+        Console.WriteLine(numbers is [_, >= 2, _, _]); // True
+    }
+
+    private void UTF_8StringLiterals()
+    {
+        /*Allows converting only UTF-8 characters to their byte representation at compile time
+         * The language will provide the u8 suffix on string literals to force the type to be UTF8.
+         * The suffix is case -insensitive, U8 suffix will be supported and will have the same meaning as u8 suffix
+         * When the u8 suffix is used, the value of the literal is a ReadOnlySpan < byte > containing a UTF-8 byte representation of the string.
+         */
+
+        // C# 10
+        byte[] array10 = Encoding.UTF8.GetBytes("Hello World");
+
+        //C# 11
+
+        var s2 = "hello"u8;                // Okay and type is ReadOnlySpan<byte>
+        ReadOnlySpan<byte> s3 = "hello"u8; // Okay.
+        byte[] s5 = "hello"u8.ToArray();   // Okay.
+        /*
+        string s1 = "hello"u8;             // Error
+        byte[] s4 = "hello"u8;             // Error - Cannot implicitly convert type 'System.ReadOnlySpan<byte>' to 'byte[]'.
+
+        Span<byte> s6 = "hello"u8;         // Error - Cannot implicitly convert type 'System.ReadOnlySpan<byte>' to 'System.Span<byte>'.
+        */
+    }
+
+    private void RawStringLiterals()
+    {
+        /*It allows containing of arbitrary text without escaping.
+         * The format is minimum 3 double quotes """.."""
+         * Combining with string interpolation, the count of $ denotes how many braces in a row start & end the interpolation.
+         */
+        string name = "Shehryar";
+        string surname = "Khan";
+
+        //C# 10
+        string jsonString10 =
+          $@"
+          {{
+            'Name': {name},
+            'Surname': {surname}
+          }}
+          ";
+
+        //C# 11
+        string jsonString11 =
+          $$"""
+          {
+            "Name": {{name}},
+            "Surname": {{surname}}
+          }
+          """;
+    }
+
+    private void RequiredMembers()
+    {
+        // Initializations with required properties 
+        var p1 = new Person { Name = "Shehryar", Surname = "Khan" };
+        Person p2 = new("Shehryar", "Khan");
+
+        // Initializations with missing required properties 
+        // var p3 = new Person { Name = "Shehryar" };
+        //  Person p4 = new();
+    }
+
+    public class Person
+    {
+        public Person() { }
+
+        [SetsRequiredMembers]
+        public Person(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+    }
 }
--- Demo.NewFeatures/Demo.Feature_CSharp12/CSharp12.cs
+++ Demo.NewFeatures/Demo.Feature_CSharp12/CSharp12.cs
@@ -1,6 +1,6 @@
 ﻿global using Demo.Feature_CSharp.Infrastructure;
+using System.Diagnostics.CodeAnalysis;
 
-//File Namespaces
 //With this feature you can define the namespace on the file level like this:
 namespace Demo.Feature_CSharp;
 
@@ -12,28 +12,130 @@ public class CSharp12 : ICSharp
         Console.WriteLine($"**************begin {this.GetType().Name} new features********************");
 
         //Primary constructors
-        PrimayConstructors();
+        PrimaryConstructors();
 
         //Collection expressions
-
-        //Inline arrays
+        CollectionExpressions();
 
         //Optional parameters in lambda expressions
-
-        //ref readonly parameters
+        OptionalParametersInLambdaExpressions();
 
         //Alias any type
+        AliasAnyType();
+
+        //Inline arrays
+        InlineArrays();
+
+        //ref readonly parameters
+        RefReadonlyParameters();
 
         //Experimental attribute
+        ExperimentalAttribute();
 
         //Interceptors
-
+        Interceptors();
 
         Console.WriteLine($"********************end {this.GetType().Name} new features********************");
     }
 
+    private void Interceptors()
+    {
+        //Interceptors are an experimental feature that allow you to substitute a call to a method with a call to another method at compile time
+    }
+
+    [Experimental("C#12 New Feature")]
+    class Foo
+    {
+        [Experimental("C#12 New Feature")]
+        public void Bar() { }
+    }
+
+    private void ExperimentalAttribute()
+    {
+        /*We can now mark types, methods, or assemblies with the ExperimentalAttribute
+        //to indicate that they are experimental features
+        //that may change or be removed in the future.
+        */
+
+        //var foo = new Foo(); // warning: 'Foo' is experimental
+        //foo.Bar(); // warning: 'Foo.Bar()' is experimental
+    }
+
+    private void RefReadonlyParameters()
+    {
+        /*use the ref readonly modifier on parameters to indicate 
+         * that they are passed by reference, but cannot be modified. 
+         * This can improve the performance of your code by avoiding unnecessary copying, 
+         * and also enforce the immutability of the arguments.
+         */
+        var one = 1;
+        var two = 2;
+
+        var max = Max(ref one, ref two);
+
+        static ref readonly int Max(ref readonly int x, ref readonly int y)
+            => ref x > y ? ref x : ref y;
+    }
+
+    [System.Runtime.CompilerServices.InlineArray(5)]
+    public struct Buffer
+    {
+        private int _element0;
+    }
+    private void InlineArrays()
+    {
+        /*Inline arrays are a way to create an array of fixed size in a struct type, 
+         * without using unsafe code or fixed size buffers.
+         */
+        var buffer = new Buffer();
+        for (int i = 0; i < 5; i++)
+        {
+            buffer[i] = i;
+        }
+    }
+
+    private void AliasAnyType()
+    {
+        //We can now use the using directive to create aliases for any type, not just named types.
+
+    }
+
+    private void OptionalParametersInLambdaExpressions()
+    {
+        //Define default values for parameters in lambda expressions, just like you can for methods or local functions
+        var incrementBy = (int source, int increment = 1) => source + increment;
+
+        Console.WriteLine(incrementBy(2)); // prints 3
+        Console.WriteLine(incrementBy(2, 3)); // prints 5
+    }
+
+    private void CollectionExpressions()
+    {
+        //Collection expressions are a new syntax to create collections of elements.
+        //Such as arrays, System.Span<T> and System.ReadOnlySpan<T>
+
+        int[] someOtherNumbers = [4, 5, 6];
+        List<int> numbers = [1, 2, 3, .. someOtherNumbers, 7, 8, 9];
+
+        string[] moreFruits = ["orange", "pear"];
+        IEnumerable<string> fruits = ["apple", "banana", "cherry", .. moreFruits];
+
+        List<(string, int)> otherScores = [("Dave", 90), ("Bob", 80)];
+        (string name, int score)[] scores = [("Alice", 90), .. otherScores, ("Charlie", 70)];
+
+        Student[] students = [new Student("Alice", 90), new Student("Bob", 80)];
+
+        Span<char> span = ['h', 'e', 'l', 'l', 'o'];
+
+        // Create a jagged 2D array from variables:
+        int[] row0 = [1, 2, 3];
+        int[] row1 = [4, 5, 6];
+        int[] row2 = [7, 8, 9];
+        int[][] twoDFromVariables = [row0, row1, row2];
+    }
+
     /// <summary>
-    /// Primay Constructors
+    /// Primary Constructors
     /// </summary>
     class Student(string name, int score)
     {
@@ -41,7 +143,7 @@ public class CSharp12 : ICSharp
         public int Score => score;
         public void Greet() => Console.WriteLine($"Hello, {name}!");
     }
-    private void PrimayConstructors()
+    private void PrimaryConstructors()
     {
         /*Primary constructors allow you to declare parameters for a class or struct
         in the same line as the type name.These parameters are in scope for the entire body of the type, 
